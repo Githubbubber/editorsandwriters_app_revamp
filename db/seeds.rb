8065273c322232671b698a815d5fe5ea611e89a1
Mekesia Brown <mekesiabrown@gmail.com>
2017-08-22 08:31:22 -0400

    Database work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Member.create(
+# 	acct_type: "w",
+# 	first_name: "w3", 
+# 	last_name: "w3", 
+# 	alter_ego: "w3", 
+# 	password_digest: "w3w3w3w3", 
+# 	email: "w3@w3.com", 
+# 	tagline: "w3", 
+# 	aboutme: "w3", 
+# 	avatar: "http://w3.net/w3.jpg", 
+# 	location: "w3", 
+# 	fav_style_manual: "", 
+# 	education: "w3", 
+# 	fav_advice: "" )
+
+
+    # t.string "acct_type", limit: 1
+    # t.string "first_name", limit: 20
+    # t.string "last_name", limit: 20
+    # t.string "alter_ego", limit: 20
+    # t.string "password_digest", limit: 20
+    # t.string "email"
+    # t.string "tagline", limit: 160
+    # t.text "aboutme", limit: 320
+    # t.string "avatar"
+    # t.string "location"
+    # t.string "fav_style_manual"
+    # t.string "education"
+    # t.string "fav_advice", limit: 160
