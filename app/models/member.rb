2d53ae9d28a5f5ae026e8f5540c8cee602f73950
Mekesia Brown <mekesiabrown@gmail.com>
2017-09-29 12:23:20 -0400

    Comment out various lines for testing

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,12 +1,12 @@
 class Member < ApplicationRecord 
-	validates :first_name, 	presence: true,
-							length: { maximum: 20 }
-	validates :last_name, 	presence: true,
-							length: { maximum: 20 }
+	# validates :first_name, 	presence: true,
+	# 						length: { maximum: 20 }
+	# validates :last_name, 	presence: true,
+	# 						length: { maximum: 20 }
 	validates :alter_ego, 	presence: true,
 							length: { maximum: 20 }
-	validates :email, 	presence: true,
-							length: { minimum: 8, maximum: 30 }
-	validates :password_digest, 	presence: true,
-							length: {  minimum: 8, maximum: 30 }
+	# validates :email, 		presence: true,
+	# 						length: { minimum: 8, maximum: 30 }
+	# validates :password_digest, 	presence: true,
+	# 						length: {  minimum: 8, maximum: 30 }
 end
